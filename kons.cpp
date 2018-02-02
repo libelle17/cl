09cc7101f117aa0bdba013d65fd876d5acaa897c
Gerald Schade <geraldschade@gmx.de>
2018-02-02 01:12:19 +0100

    Version 0.10305

--- kons.cpp
+++ kons.cpp
@@ -201,6 +201,10 @@ const char *kons_T[T_konsMAX+1][SprachZahl]=
   {"Programm '","Program '"},
   // T_laeuft_schon_einmal_Breche_ab
   {"' laeuft schon einmal. Breche ab.","' is running already once. Aborting."},
+	// T_laueft_schon_einmal_aber
+	{"' laeuft schon einmal, aber","' is running already once, but"},
+	// T_wird_deshalb_abgebrochen
+	{"wird deshalb abgebrochen.","will be killed therefore."},
   // T_Wert
   {" Wert: "," Value: "},
   // T_Dauer
@@ -2539,33 +2543,42 @@ void pruefplatte()
 // ob das aktuelle Programm mehrfach laeuft; bei obstumm Exit-Code 0
 void pruefmehrfach(const string& wen,uchar obstumm/*=0*/)
 {
+	const long smax=3600; // maximal tolerierte Sekundenzahl, bevor statt dem eigenen Prozess der andere abgebrochen wird
 	svec rueck;
 	const string iwen=wen.empty()?base_name(meinpfad()):wen;
-	for(int iru=0;iru<3;iru++) {
-		systemrueck("ps -eo comm,cputime,pid|grep -P '^"+iwen+"([[:space:]]|\\z)'",0,0,&rueck,/*obsudc=*/0);
-		if (rueck.size()>1) {
-			// z.B. 'autofax      18:02:48  2939'
-			svec pvec;
-			aufSplit(&pvec,rueck[0],' ',/*auchleer=*/0); 
-			caus<<"pvec.size(): "<<pvec.size()<<endl;
-			for(unsigned i=0;i<pvec.size();i++) {
-				caus<<"i: "<<i<<", pvec[i]: "<<pvec[i]<<endl;
-			}
-			if (pvec.size()>2) {
-				if (pvec[1].find("00:00")) {    // wenn es mindestens eine Stunde laeuft
-					if (iru)
-						systemrueck("pkill -9 "+iwen,1);
-					else
-						systemrueck("pkill "+iwen,1);
-					continue;
-				}
-			}
+	systemrueck("ps -eo comm,cputime,pid|grep -P '^"+iwen+"([[:space:]]|\\z)'",!obstumm,0,&rueck,/*obsudc=*/0);
+	for(int aru=0;aru<3;aru++) {
+		if (rueck.size()==1) // ich
+			break;
+		if (aru<2) {
+			for(unsigned iru=0;iru<rueck.size();iru++) {
+				// z.B. 'autofax      18:02:48  2939'
+				svec pvec;
+				aufSplit(&pvec,rueck[iru],' ',/*auchleer=*/0); 
+				if (pvec.size()>2) {
+					long sek=0;
+					if (pvec[1].size()==8) {
+						int h,m,s;
+						sscanf(pvec[1].c_str(),"%2i:%2i:%2i",&h,&m,&s);
+						sek=3600*h+60*m+s;
+					} // 				if (pvec[1].size()==8)
+					if (sek>smax) {    // wenn es mindestens eine Stunde laeuft
+						cout<<Txk[T_Program]<<blau<<iwen<<schwarz<<Txk[T_laueft_schon_einmal_aber]<<" "<<rot<<sek<<schwarz<<" s (> "<<blau<<smax<<schwarz<<" s), "<<Txk[T_wird_deshalb_abgebrochen]<<endl;
+						if (!systemrueck(string("kill ")+(aru?"-9 ":"")+pvec[2]+" 2>/dev/null",!obstumm,0,0,/*obsudc=*/1)) {
+							rueck.erase(rueck.begin()+iru);
+							continue;
+						}
+					} // 				if (sek>15)
+				} // 			if (pvec.size()>2)
+			} // 		for(int iru=0;iru<rueck.size();iru++)
+			// if (aru<2)
+		} else { 
 			if (obstumm)
 				exit(0);
 			cout<<Txk[T_Program]<<blau<<iwen<<schwarz<<Txk[T_laeuft_schon_einmal_Breche_ab]<<endl;
 			exit(98);
-		} //   if (rueck.size()>1)
-	}
+		} // if (aru<2) else
+	} // 	for(int aru=0;aru<3;aru++) 
 	/*//
 		for(size_t j=0;j<rueck.size();j++) KLA
 	// <<violett<<"rueck["<<j<<"]: "<<rot<<rueck[j]<<schwarz<<endl;
